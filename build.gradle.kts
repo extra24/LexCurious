4d5c7e38cec846a943a44f76f4fba75716cfe535
aram <6aram@naver.com>
2025-07-12 07:35:42 +0900

    gradle 설정 수정, CRLF 변환

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,16 +2,25 @@ plugins {
     id("java")
 }
 
-group = "org.example"
+group = "com.lexqurious"
 version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17) // JDK 17 설정
+    }
+}
+
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    //@TODO HTTP 클라이언트
+    //@TODO JSON 파싱 라이브러리
 }
 
 tasks.test {
