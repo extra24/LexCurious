05d44e48a2bd3902bed27ff5cd45b1d09eeb8ea4
smhong <smhong@rtk.ai>
2025-07-12 16:31:06 +0900

    swing ui designer 연동

--- src/main/java/com/lexcurious/LexCuriousManager.java
+++ src/main/java/com/lexcurious/LexCuriousManager.java
@@ -1,9 +1,12 @@
 package com.lexcurious;
 
+import com.lexcurious.ui.MainFrame;
+
 //메인
 public class LexCuriousManager {
 
     public static void main(String[] args) {
-
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
     }
 }
