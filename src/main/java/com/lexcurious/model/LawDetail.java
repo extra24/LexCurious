5594ce1abaed5cbe98bfa7b1cc3674caaf30e30c
aram <6aram@naver.com>
2025-07-12 14:27:57 +0900

    LawDetail model

--- src/main/java/com/lexcurious/model/LawDetail.java
+++ src/main/java/com/lexcurious/model/LawDetail.java
@@ -1,4 +1,11 @@
 package com.lexcurious.model;
 
-public class LawDetail {
+import com.google.gson.annotations.SerializedName;
+
+// 법령 본문 정보 모델 : 현행법령 본문 모델
+public record LawDetail(
+        @SerializedName("법령ID") int lawId,
+        @SerializedName("법령명_한글") String lawName,
+        @SerializedName("조문내용") String lawContent
+) {
 }
